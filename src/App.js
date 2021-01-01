d121f722de3941ae566fabccb3b99b57e67be1b8
yashar-akish <yashar_akish@yahoo.com>
2021-01-01 13:01:52 -0500

    tring to using React Hooks in App instead of using class based component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+//import React, { useState } from 'react';
 import './App.css';
 
 import Person from './Components/Person/Person';
 
+
+
+/**
+ *      using class and state:
+ */
 
 class App extends Component {
 
@@ -12,10 +18,12 @@ class App extends Component {
             { name: 'Monica', age: 22 },
             { name: 'Jane', age: 27 },
         ],
-    }
+        otherState: 'some other value'
+    };
 
     switchNameHandler = () => {
         //console.log('was clicked')
+        // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
         this.setState({
             persons: [
                 { name: 'Jason', age: 22 },
@@ -52,5 +60,68 @@ class App extends Component {
         /*return (React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'nested createElement with css')));*/
     }
 }
-
 export default App;
+
+
+/**
+ *      using React Hook:
+ */
+
+// const App = props => {
+
+//     const [personsState, setPersonsState] = useState({
+//         persons: [
+//             { name: 'Max', age: 28 },
+//             { name: 'Monica', age: 22 },
+//             { name: 'Jane', age: 27 },
+//         ],
+//         //otherState: 'some other value'
+//     });
+
+//     //useState({ otherState: 'some other value' })
+//     const [otherState, setOtherState] = useState('some other value')
+
+//     console.log(personsState, otherState);
+
+//     const switchNameHandler = () => {
+//         //console.log('was clicked')
+//         // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+//         setPersonsState({
+//             persons: [
+//                 { name: 'Jason', age: 22 },
+//                 { name: 'Elena', age: 20 },
+//                 { name: 'Jane', age: 21 },
+//             ],
+//             //otherState: personsState.otherState
+//         })
+//     }
+
+//     return (
+//         <div className="App">
+//             <h1>Let's Start</h1>
+//             <button onClick={switchNameHandler}>Switch Name</button>
+//             <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//             <Person name={personsState.persons[1].name} age={personsState.persons[1].age}> - My hobbies: racing</Person>
+//             <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+//         </div>
+//     );
+
+//     // return (
+//     //     <div className="App">
+//     //         <h1>Let's Start</h1>
+//     //         <button>Switch Name</button>
+//     //         <Person name='Max' age='30' />
+//     //         <Person name='Manu' age='24'> - My hobbies: racing</Person>
+//     //         <Person name='Stephanie' age='19' />
+//     //     </div>
+//     // );
+
+//     /* return (React.createElement('div', null, 'h1', 'createElement')); */
+
+//     /*return (React.createElement('div', null, React.createElement('h1', null, 'nested createElement')));*/
+
+//     /*return (React.createElement('div', {className: 'App' }, React.createElement('h1', null, 'nested createElement with css')));*/
+
+// }
+
+// export default App;
