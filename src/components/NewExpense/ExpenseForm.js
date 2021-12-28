23e9fff7d3e425b2328ca19cedd5b651f46ba663
Yashar Akish <yashar_akish@yahoo.com>
2021-12-27 20:20:42 -0500

    using another form of using useState whenever state update depends on previous update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -20,26 +20,43 @@ const ExpenseForm = () => {
 
 
     const titleChangeHandler = (event) => {
+
         //setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle: event.target.value
+        // });
+
+        //----whenever our state update depends on previous update use this form-----//
+        setUserInput((prevState) => {
+            return {...prevState, enteredTitle: event.target.value}
         });
     };
 
     const amountChangeHandler = (event) => {
         //setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: event.target.value
+        // });
+
+        setUserInput((prevState) => {
+            return {...prevState, enteredAmount: event.target.value}
         });
     };
 
     const dateChangeHandler = (event) => {
         //setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: event.target.value
+        // });
+
+        setUserInput((prevState) => {
+            return {...prevState, enteredDate: event.target.value}
         });
     };
 
